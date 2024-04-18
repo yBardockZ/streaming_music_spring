e71f4897f8299d6052222aba291ea690b69ea4ee
Thalles <thalles_leopoldino@outlook.com>
2024-04-18 16:16:11 -0300

    Project created.

---
+++ src/main/java/com/ybardockz/streaming_music/StreamingMusicApplication.java
@@ -0,0 +1,13 @@
+package com.ybardockz.streaming_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ybardockz/streaming_music/StreamingMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ybardockz.streaming_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
